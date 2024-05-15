1e7a0541b2301226874f4ecc8a382e8f5a09b02e
usER <usER@THINKPAD>
2024-05-16 03:30:54 +0700

    Add project files.

---
+++ Views/Pengepul/RegistrasiPengepul.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Ngupy.Views.Pengepul
+{
+    partial class RegistrasiPengepul
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2CustomGradientPanel1 = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.SuspendLayout();
+            // 
+            // guna2CustomGradientPanel1
+            // 
+            this.guna2CustomGradientPanel1.Location = new System.Drawing.Point(645, 140);
+            this.guna2CustomGradientPanel1.Name = "guna2CustomGradientPanel1";
+            this.guna2CustomGradientPanel1.Size = new System.Drawing.Size(0, 0);
+            this.guna2CustomGradientPanel1.TabIndex = 1;
+            // 
+            // RegistrasiPengepul
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1418, 794);
+            this.Controls.Add(this.guna2CustomGradientPanel1);
+            this.Name = "RegistrasiPengepul";
+            this.Text = "Registrasi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel guna2CustomGradientPanel1;
+    }
+}
